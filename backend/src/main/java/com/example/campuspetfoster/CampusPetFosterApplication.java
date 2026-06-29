602da7fe4c71411d1a36a14922804020c4433c95
sheep <2199807644@qq.com>
2026-06-29 15:17:58 +0800

    fix: simplify db config with native spring env vars

--- backend/src/main/java/com/example/campuspetfoster/CampusPetFosterApplication.java
+++ backend/src/main/java/com/example/campuspetfoster/CampusPetFosterApplication.java
@@ -1,6 +1,5 @@
 package com.example.campuspetfoster;
 
-import com.example.campuspetfoster.config.DatabasePropertyInitializer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,8 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class CampusPetFosterApplication {
 
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(CampusPetFosterApplication.class);
-        application.addInitializers(new DatabasePropertyInitializer());
-        application.run(args);
+        SpringApplication.run(CampusPetFosterApplication.class, args);
     }
 }
